5b345d1956a87313d7e8d44bc38876ea477e4a86
unknown <nascimento.joao.dev@gmail.com>
2024-08-03 10:34:19 -0300

    initial structure

---
+++ painel-api/src/main/java/com/brainday/painelapi/PainelApiApplication.java
@@ -0,0 +1,13 @@
+package com.brainday.painelapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PainelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PainelApiApplication.class, args);
+	}
+
+}
---
+++ painel-api/src/test/java/com/brainday/painelapi/PainelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brainday.painelapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PainelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
